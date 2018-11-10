ac8ac5eba9e3fec2aa77145970d6bd79c5a23edc
Vivien De saint-exupery <vde-sain@z3r7p2.le-101.fr>
2018-11-10 18:40:02 +0100

    fillit breakthrough

--- check_tetro_placement.c
+++ check_tetro_placement.c
@@ -6,85 +6,139 @@
 /*   By: vde-sain <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/11/08 11:55:23 by vde-sain     #+#   ##    ##    #+#       */
-/*   Updated: 2018/11/09 15:01:30 by vde-sain    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/11/10 18:39:47 by vde-sain    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-char			*ft_find_form(t_fillist *list, int x, int y, char *form)
-{
-	int			hash_nb;
-	int			i;
-	int			done;
-	char		tetro_symb;
+/*
+** CO = COORDINATES AND INDEX (because of norm):
+** co[0] = x
+** co[1] = y
+** co[2] = index
+** co[3] = done
+*/
 
-	hash_nb = 0;
-	i = 0;
-	done = 0;
-	tetro_symb = 'A' + list->tetro_nb;
-	while (done == 0 && y < 4 && x < 4)
+static char			*ft_fill_form(char **tetros, int *co, char *form, 
+					char t_symb)
+{
+	while (co[3] == 0 && co[1] < 4 && co[0] < 4)
 	{
-		while (list->tetros[y][x + 1] == (tetro_symb) && done == 0 && x < 4)
+		while (tetros[co[1]][co[0] + 1] == t_symb && co[0]++ >= 0)
+			form[co[2]++] = 'd';
+		while (tetros[co[1]][co[0] - 1] == t_symb && co[3] == 0 && co[0]-- >= 0)
+			form[co[2]++] = 'g';
+		co[3]++;
+		while (tetros[co[1]][co[0] + 1] == t_symb && co[3] == 1)
 		{
-			ft_putstr("droite\n");
-			form[i++] = 'd';
-			x++;
+			if (tetros[co[1] + 1] && tetros[co[1] + 1][co[0]] == t_symb)
+			{
+				form[co[2]++] = 'b';
+				co[1]++;
+				co[3] = 0;
+				break ;
+			}
+			form[co[2]++] = 'd';
+			co[0]++;
 		}
-		done++;
-		while (list->tetros[y][x - 1] == ('A' + list->tetro_nb) && done == 1)
-		{
-			ft_putstr("\ngauche\n");
-			form[i++] = 'g';
-			x--;
-		}
-		done++;
-		if (list->tetros[y + 1][x] == ('A' + list->tetro_nb))
-		{
-			ft_putstr("\nbas\n");
-			form[i++] = 'b';
-			y++;
-			done = 0;
-		}
-		form[i] = '\0';
-	printf("APRES:\ndone = %d\ny = %d\nx = %d\ni = %d\nform[%d] = %c\n\n\n", done, y, x, i, i - 1, form[i - 1]);
+		if (tetros[co[1] + 1] && tetros[co[1] + 1][co[0]] == t_symb &&
+				co[1]++ >= 0 && co[3]-- >= 0)
+			form[co[2]++] = 'b';
 	}
+	form[co[2]] = '\0';
 	return (form);
 }
 
-char			*ft_verif_form(t_fillist *list, char *form)
+static char			*ft_find_form(t_fillist *list, int x, int y, char *form)
+{
+	char		t_symb;
+	int			co[4];
+
+	t_symb = 'A' + list->tetro_nb;
+	co[0] = x;
+	co[1] = y;
+	co[2] = 0;
+	co[3] = 0;
+	form = ft_fill_form(list->tetros, co, form, t_symb);
+	return (form);
+}
+
+static char			*ft_verif_form(t_fillist *list, char *form)
 {
 	int			y;
 	int			x;
 	int			i;
+	char		tetro_symb;
 
+	tetro_symb = 'A' + list->tetro_nb;
 	y = 0;
 	i = 0;
-	while (list->tetros[y] && list->tetros[y][x] != 'A' + list->tetro_nb)
+	x = 0;
+	while (list->tetros[y] && list->tetros[y][x] != ('A' + list->tetro_nb))
 	{
 		x = 0;
-		while (list->tetros[y][x] != ('A' + list->tetro_nb) && x < 4)
+		while (list->tetros[y][x] != 'A' + list->tetro_nb && x < 4)
 			x++;
-		if (list->tetros[y][x] != ('A' + list->tetro_nb))
+		if (list->tetros[y][x] != 'A' + list->tetro_nb)
 			y++;
 	}
 	if (!(form = ft_strnew(9)))
 		return (NULL);
-	printf("BEFORE:x = %d\ny = %d\n", x, y);
 	form = ft_find_form(list, x, y, form);
 	return (form);
 }
 
-int				ft_init_verif(char **res, t_fillist *list, int ver, int hor)
+int				ft_verif_place(char **res, int y, int x, char *form)
+{
+	int			i;
+	int			check;
+
+	i = -1;
+	check = 0;
+	while (++i >= 0 && res[y] && res[y][x] && form[i])
+	{
+		if (res[y][x + 1] && form[i] == 'd' && res[y][x + 1] == '.' && x++ >= 0)
+			check++;
+		if (res[y][x - 1] && form[i] == 'g' && res[y][x - 1] == '.' && x-- >= 0)
+			check++;
+		if (res[y + 1] && form[i] == 'b' && res[y + 1][x] == '.' && y++ >= 0)
+			check++;
+	}
+	if (check == (int)ft_strlen(form))
+	{
+		printf("return1\n");
+			return (1);
+	}
+	printf("return-1\n");
+	return (-1);
+}
+
+int				ft_init_verif(char **res, t_fillist *list, int y, int x)
 {
 	char		*form;
+	char		tetro_symb;
+	int			i;
 
-	res = 0;
-	ver = 0;
-	hor = 0;
+	i = 0;
+	tetro_symb = 'A' + list->tetro_nb;
 	form = NULL;
 	form = ft_verif_form(list, form);
-	printf("form = %s\n", form);
-	return (1);
+	if (ft_verif_place(res, y, x, form) == 1)
+	{
+		res[y][x] = tetro_symb;
+		while (form[i])
+		{
+			if (form[i] == 'd')
+				res[y][++x] = tetro_symb;
+			if (form[i] == 'g')
+				res[y][--x] = tetro_symb;
+			if (form[i] == 'b')
+				res[++y][x] = tetro_symb;
+			i++;
+		}
+		return (1);
+	}
+	return (-1);
 }
--- solve_fillit.c
+++ solve_fillit.c
@@ -6,82 +6,18 @@
 /*   By: vde-sain <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/11/07 15:22:36 by vde-sain     #+#   ##    ##    #+#       */
-/*   Updated: 2018/11/09 13:24:27 by vde-sain    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/11/10 18:38:05 by vde-sain    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-/*static int		ft_calculate_dist(t_fillist *list, int first_pos, int left_pos)
-{
-	int			hor;
-	int			ver;
-
-	hor = 0;
-	ver = 0;
-	while (list->tetros[ver])
-	{
-		hor = 0;
-		while (list->tetros[ver][hor])
-		{
-			if (list->tetros[ver][hor] >= 'A' && list->tetros[ver][hor] <= 'Z')
-			{
-				if (first_pos > 3)
-					first_pos = hor;
-				else if (hor < left_pos)
-					left_pos = hor;
-			}
-				hor++;
-		}
-		ver++;
-	}
-	if (first_pos - left_pos > 0)
-		return (first_pos - left_pos);
-	else
-		return (0);
-}
-
-static char		**ft_place_tetro(char **res, t_fillist *list, int ver, int hor)
-{
-	int			dist;
-	int			tab_ver;
-	int			tab_hor;
-	int			tetros_symb;
-
-	tab_ver = 0;
-	tetros_symb = 0;
-	printf("dist = %d\n", dist = ft_calculate_dist(list, 4, 4));
-
-	while (tetros_symb < 4 && list->tetros[tab_ver])
-	{
-		tab_hor = 0;
-		while (list->tetros[tab_ver][tab_hor])
-		{
-			ft_putstr("bouh");
-			if (list->tetros[tab_ver][tab_hor] == 'A' + list->tetro_nb)
-			{
-				if ((hor + dist) <= (int)ft_strlen(list->tetros[tab_ver]))
-				{
-					res[ver][hor + dist] = list->tetros[tab_ver][tab_hor];
-					tetros_symb++;
-				}
-				else
-					return (NULL);
-			hor++;
-			tab_hor++;
-			}
-		tab_ver++;
-		ver++;
-		}
-	}
-	return (res);
-}*/
-
 char			**ft_initiate_fill_res_tab(t_fillist *list, char **res)
 {
 	int			hor;
 	int			ver;
+	int			i = 0;
 
 	ver = 0;
 	while (res[ver] && list != NULL)
@@ -93,13 +29,24 @@ char			**ft_initiate_fill_res_tab(t_fillist *list, char **res)
 			{
 				if ((ft_init_verif(res, list, ver, hor)) == 1)
 				{
-					//res = ft_place_tetro(res, list, ver, hor);
+					hor = 0;
+					ver = 0;
+					i = 0;
+					while (res[i])
+					{
+						printf("res[%d] = %s\n", i, res[i]);
+						i++;
+					}
 					list = list->next;
 				}
 			}
 			hor++;
 		}
 		ver++;
+	}
+	if (ver == (int)ft_strlen(res[ver - 1]))
+	{
+		ft_putstr("au secours");
 	}
 	return (res);
 }
